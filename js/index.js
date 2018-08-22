47c978267e4fdd10a0dd59c749fdf6dbd3587a99
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-08-23 00:16:31 +0200

    General adjust

--- js/index.js
+++ js/index.js
@@ -11,8 +11,10 @@
     });
   }
 
-  //game
-  //global variables
+//game
+
+
+//global variables
 var params = {winsNumber: 0, roundNumber: 0, playerScore: 0, computerScore: 0, canPlay: false};
 
 newGame.addEventListener('click', function(event){
@@ -75,4 +77,5 @@ var playerMoveAction = function(playerMove){
       displayGameResult(message, message2);
     }
 };
-})(); 
+
+})();
