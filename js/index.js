38f0fa12f94cbde04a75cf252e81c93d3b365fb6
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-08-22 18:53:36 +0200

    Finish stage-2

--- js/index.js
+++ js/index.js
@@ -1,157 +1,83 @@
+'use strict';
+(function(){
+	
+	// Mimo, że obecnie mamy tylko jeden link, stosujemy kod dla wielu linków. W ten sposób nie będzie trzeba go zmieniać, kiedy zechcemy mieć więcej linków lub guzików otwierających modale
+	
+	var buttonsFinder = document.querySelectorAll('.player-move');
+	
+	for(var i = 0; i < buttonsFinder.length; i++){
+		buttonsFinder[i].addEventListener('click', function(event){
+  playerMoveAction(this.getAttribute('data-move'));
+    });
+  }
+})(); 
+
 'use strict'
+
 //global variables
 var winsNumber = 0,
     roundNumber = 0,
-    score1 = 0,
-    score2 = 0,
-    score,
-    play = true,
-    buttonPaper = document.getElementById('buttonPaper'),
-    buttonScissors = document.getElementById('buttonScissors'),
-    buttonStone = document.getElementById('buttonStone');
+    playerScore = 0,
+    computerScore = 0,
+    canPlay = false;
 
-//buttons EventsListeners
-  buttonPaper.addEventListener('click', function(event){
-    displayGameResult(playerMove(computerMove(), 'Papier'), resultsArray());
-  });
-
-  buttonScissors.addEventListener('click', function(event){
-    displayGameResult(playerMove(computerMove(), 'Nożyce'), resultsArray());
-  });
-
-  buttonStone.addEventListener('click', function(event){
-    displayGameResult(playerMove(computerMove(), 'Kamień'), resultsArray());
-  });
-
-  newGame.addEventListener('click', function(event){
-    winsNumber = window.prompt('Chcesz zagrać kolejny raz? Do ilu gramy?!'),
-    play = true;
-    roundNumber = 0,
-    score1 = 0,
-    score2 = 0;
-    displayGameResult('','');
-  });
+newGame.addEventListener('click', function(event){
+  var newWinNumber = window.prompt('Chcesz zagrać kolejny raz? Do ilu gramy?!');
+  if (!newWinNumber) {
+    return false;
+  }
+  winsNumber = newWinNumber;
+  canPlay = true;
+  roundNumber = 0;
+  playerScore = 0;
+  computerScore = 0;
+  displayGameResult('','');
+});
 
 //functions
 var displayGameResult = function(textToDisplay, roundResult){
-  var gameResult = document.getElementById('gameResult');
-  var result = document.getElementById('result');
-    gameResult.innerHTML = textToDisplay;
-    result.innerHTML = roundResult;
+  document.getElementById('gameResult').innerHTML = textToDisplay;
+  document.getElementById('result').innerHTML = roundResult;
 }
 
-var whoWon = function(){
-  var message;
+var getComputerMove = function(){
+  var optionsArray = ['paper', 'scissors', 'stone'];
+  var moveNumber = Math.floor(3 * Math.random());
   
-  if (score1 > score2){
-    score = score1;
-    message = ('GAME OVER! YOU WON! Punkty które zdobyłeś: ' + score);
-    score = score1;
-  }
-  
-  if (score1 < score2){
-    score = score2;
-    message = ('GAME OVER! Twój przeciwnik zwyciężył! Punkty które zdobył: ' + score);
-    score = score2;
-  }
-  
-  if (winsNumber = 'null' && score1 == 0 && score2 == 0){
-    message = ('Coś namieszałeś... Wykonaj wszystko ponownie, tym razem zgodnie z instrukcją!');
-    play = false;
-  }
-  
-  return message;
-  
-}
-
-var matchTrial = function(){
-  if (winsNumber != 0 && (winsNumber == score1 || winsNumber == score2)){
-    displayGameResult(whoWon(), '');
-    play = false;
-  }
-  
-  else if (winsNumber == 0){
-    play = false;
-    roundNumber = 0,
-    score1 = 0,
-    score2 = 0;
-  }
-  
-  else if (winsNumber > score1 && winsNumber > score2){
-    play = true;
-  }
-  
-  else if (winsNumber = 'null'){
-    play = false
-  }
-
-}
-
-var resultsArray = function(){
-  var message;
-  message = ('<strong>Round</strong> no ' + roundNumber + '<br>' + '<strong>Player1 result <br></strong>' + score1 + '<br>' + '<strong>Player2 result</strong> <br>' + score2 + '<br> Gramy do ' + winsNumber);
-  return message;
-}
-
-var computerMove = function(){
-  var moveNumber = Math.round(2 * Math.random() + 1);
-  var computer;
-  if (moveNumber == 1){
-    computer = 'Papier';
-  }
-  if (moveNumber == 2){
-    computer = 'Nożyce';
-  }
-  if (moveNumber == 3){
-    computer = 'Kamień';
-  }
-  return computer;
+  return optionsArray[moveNumber];
 }
 
 //main function
-var playerMove = function(player1, player2, liczbaRund){
-  matchTrial();
+var playerMoveAction = function(playerMove){
+  if (!canPlay){
+    alert('Chcesz zagrać? Kliknij New Game i postępuj zgodnie z komunikatem!');
+    return false;
+  }
   roundNumber++;
-  var message;
-  if (play == true){
-  if (player1 === player2){
-      message = ('Remis! Wybrałeś ' + player2 + '! Twój przeciwnik zagrał ' + player1 + '! <br>');
-  }
-  //player1 wins
-  else if (player2 == 'Papier' && player1 == 'Kamień'){
-    message = ('Wygrałeś! <br> Wybrałeś ' + player2 + '! Twój przeciwnik zagrał ' + player1 + '! <br>');
-    score1++;
-  }
-  
-  else if (player2 == 'Nożyce' && player1 == 'Papier'){
-    message = ('Wygrałeś! <br> Wybrałeś ' + player2 + '! Twój przeciwnik zagrał ' + player1 + '! <br>');
-    score1++;
-  }
-  
-  else if (player2 == 'Kamień' && player1 == 'Nożyce'){
-    message = ('Wygrałeś! <br> Wybrałeś ' + player2 + '! Twój przeciwnik zagrał ' + player1 + '! <br>');
-    score1++;
-  }
-  //Player 2 wins
-  else if (player2 == 'Kamień' && player1 == 'Papier'){
-    message = ('Przegrałeś! <br> Wybrałeś ' + player2 + '! Twój przeciwnik zagrał ' + player1 + '! <br>');
-    score2++;
-  }
-  
-  else if (player2 == 'Nożyce' && player1 == 'Kamień'){
-    message = ('Przegrałeś! <br> Wybrałeś ' + player2 + '! Twój przeciwnik zagrał ' + player1 + '! <br>');
-    score2++;
-  }
-  
-  else if (player2 == 'Papier' && player1 == 'Nożyce'){
-    message = ('Przegrałeś! <br> Wybrałeś ' + player2 + '! Twój przeciwnik zagrał ' + player1 + '! <br>');
-    score2++;
-  }
-  
-  return message;
-  }
-  else {
-    alert(whoWon() + ' Chcesz zagrać kolejny raz? Kliknij New Game!');
-    display('','')
-  }
+  var computerMove = getComputerMove(),
+      message,
+      message2;
+  //poniżej należy wstawić sprawdzanie kto wygrał
+    if (playerMove == computerMove){
+      message = ('Remis! Wybrałeś ' + playerMove + '! Twój przeciwnik zagrał ' + computerMove + '! <br>');
+    } else if ((playerMove == 'paper' && computerMove == 'stone') || (playerMove == 'scissors' && computerMove == 'paper') || (playerMove == 'stone' && computerMove == 'scissors')){
+      message = ('Wygrałeś! <br> Wybrałeś ' + playerMove + '! Twój przeciwnik zagrał ' + computerMove + '! <br>');
+      playerScore++;
+    } else {
+      message = ('Przegrałeś! <br> Wybrałeś ' + playerMove + '! Twój przeciwnik zagrał ' + computerMove + '! <br>');
+      computerScore++;
+    };
+  
+    if (playerScore == winsNumber){
+      message2 = ('GAME OVER! YOU WON! Punkty które zdobyłeś:<br>' + playerScore);
+      canPlay = false;
+      displayGameResult('', message2);
+    } else if (computerScore == winsNumber){
+      message2 = ('GAME OVER! Twój przeciwnik zwyciężył! Punkty które zdobył:<br> ' + computerScore);
+      canPlay = false;
+      displayGameResult('', message2);
+    } else {
+      message2 = ('<strong>Round</strong> no ' + roundNumber + '<br>' + '<strong>Player1 result <br></strong>' + playerScore + '<br>' + '<strong>Player2 result</strong> <br>' + computerScore + '<br> Gramy do ' + winsNumber);
+      displayGameResult(message, message2);
+    }
 };
