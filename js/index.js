8413503ce390499677d5d16cf02751255d57e9da
Gregorion Skquarna <gs.gregorion@gmail.com>
2018-09-02 00:36:06 +0200

    Finish stage-6

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 'use strict';
 (function() {
-	//przypisuje funkcje playerMoveAction do odpowiednich buttonów: papier, nożyce, kamień
+	//button paper, scissors, stone binding to the function playerMoveAction
 	var buttonsFinder = document.querySelectorAll('.player-move');
 	
 	  for (var i = 0; i < buttonsFinder.length; i++) {
@@ -9,14 +9,23 @@
       });
     }
   
-//show and hide Modal
+  //show and hide Modal
   var handleShowModal = function(event) {
     event.preventDefault();
     var modalToOpen = (this.getAttribute('href')); //gpsE - getting href value from link
       document.querySelector('#modal-overlay').classList.add('show');
-      document.querySelector(modalToOpen).classList.add('show'); //gpsE - query serach to add show class
+      document.querySelector(modalToOpen).classList.add('show');
   }
+  
+  //close function binding to close button
+  var modals = document.querySelectorAll('.modal');
 
+    for (var i = 0; i < modals.length; i++) {
+      modals[i].addEventListener('click', function(event) {
+        event.stopPropagation();
+      });
+    }
+  
   var handleHideModal = function(event) {
     event.preventDefault();
  
@@ -26,30 +35,21 @@
     document.querySelector('#modal-overlay').classList.remove('show'); // overlay close
   }
 
-// modalLinks finder
+  //modalLinks finder
   var modalLinks = document.querySelectorAll('.show-modal');
 
     for (var i = 0; i < modalLinks.length; i++) {
       modalLinks[i].addEventListener('click', handleShowModal);  
     }
 
-//close function binding to close button
-  var modals = document.querySelectorAll('.modal'); // gpse - variable to use in for loop to find all modals
-
-    for (var i = 0; i < modals.length; i++) {
-      modals[i].addEventListener('click', function(event) {
-        event.stopPropagation();
-      });
-    }
-
-//close modal
+  //close modal
   var closeButtons = document.querySelectorAll('.modal .close');
 
     for (var i = 0; i < closeButtons.length; i++) {
       closeButtons[i].addEventListener('click', handleHideModal);
     }
 
-//close to open modal
+  //close to open modal
   var displayFinalResultTable = function() {
     document.querySelector("#modal-final-result").classList.remove('show');
     tableResult = document.querySelector('#modal-table').classList.add('show');
@@ -61,11 +61,11 @@
       closeOpenButtons[i].addEventListener('click', displayFinalResultTable);
     }
 
-// closing through the overlay clicking
+  //closing through the overlay clicking
   document.querySelector('#modal-overlay').addEventListener('click', handleHideModal);
 
-//game
-//global variables
+//main game
+  //global variables
   var params = {
       winsNumber: 0, 
       roundNumber: 0, 
@@ -73,30 +73,50 @@
       computerScore: 0,
       data: [],
       canPlay: false,
+      playerName: 'Human Player',
+      newWinNumber: 5,
       progress: []
   };
 
   //functions
   //new game button function
   newGame.addEventListener('click', function(event) {
-    var newWinNumber = window.prompt('Chcesz zagrać kolejny raz? Do ilu gramy?!');
-    if (!newWinNumber || isNaN(newWinNumber)) {
-      return false;
-    }
-
-    params.winsNumber = newWinNumber;
-    params.canPlay = true;
-    params.roundNumber = 0;
-    params.playerScore = 0;
-    params.computerScore = 0;
-    params.progress = [];
-    displayGameResult('','');
-    
-    for (var i = 0; i < buttonsFinder.length; i++) {
-      buttonsFinder[i].disabled = false;
-      buttonsFinder[i].classList.remove('player-move-disabled');
-    }
+    document.querySelector('#modal-overlay').classList.add('show');
+    document.querySelector('#modal-new-game').classList.add('show');
   });
+
+  //player name and round number to play establish
+  var gettingPlayerNameNewWinNumber = function() {
+    params.playerName = document.getElementById('player-name').value;
+    params.newWinNumber = document.getElementById('round-number').value;
+  }
+  
+  //starting game
+  var startButton = document.getElementById('start-button');
+    startButton.addEventListener('click', function(event) {
+      
+      gettingPlayerNameNewWinNumber();
+
+        document.querySelector('#modal-overlay').classList.remove('show');
+        document.querySelector('#modal-new-game').classList.remove('show');
+  
+      if (!params.newWinNumber || isNaN(params.newWinNumber)) {
+        return false;
+      }
+
+      params.winsNumber = params.newWinNumber;
+      params.canPlay = true;
+      params.roundNumber = 0;
+      params.playerScore = 0;
+      params.computerScore = 0;
+      params.progress = [];
+      displayGameResult('','');
+    
+      for (var i = 0; i < buttonsFinder.length; i++) {
+        buttonsFinder[i].disabled = false;
+        buttonsFinder[i].classList.remove('player-move-disabled');
+      }
+    });
 
   //display round result
   var displayGameResult = function(textToDisplay, roundResult) {
@@ -144,7 +164,7 @@
       buttonsFinder[i].classList.add('player-move-disabled');
     }
   }
-
+ 
   //main function
   var playerMoveAction = function(playerMove) {
     if (!params.canPlay) {
@@ -163,31 +183,31 @@
   //result check
   if (playerMove == computerMove) {
     whoWin = 'Draw';
-    message = 'Remis! Wybrałeś ' + playerMove + '! Twój przeciwnik zagrał ' + computerMove + '! <br>';
+    message = 'Remis! <br>' + params.playerName + ' zagrał ' + playerMove + '! <br> Twój przeciwnik zagrał ' + computerMove + '! <br>';
   } else if ((playerMove == 'paper' && computerMove == 'stone') || (playerMove == 'scissors' && computerMove == 'paper') || (playerMove == 'stone' && computerMove == 'scissors')){
     params.playerScore++;
     whoWin = 'Player';
-    message = 'Wygrałeś! <br> Wybrałeś ' + playerMove + '! Twój przeciwnik zagrał ' + computerMove + '! <br>';
+    message = 'Wygrałeś! <br>' + params.playerName + ' zagrał ' + playerMove + '! <br> Twój przeciwnik zagrał ' + computerMove + '! <br>';
   } else {
     params.computerScore++;
     whoWin = 'Computer';
-    message = 'Przegrałeś! <br> Wybrałeś ' + playerMove + '! Twój przeciwnik zagrał ' + computerMove + '! <br>';
+    message = 'Przegrałeś! <br>' + params.playerName + ' zagrał ' + playerMove + '! <br> Twój przeciwnik zagrał ' + computerMove + '! <br>';
   };
   
   if (params.playerScore == params.winsNumber) {
-    message2 = 'GAME OVER! YOU WON! Punkty które zdobyłeś:<br>' + params.playerScore;
+    message2 = 'GAME OVER! <br>' + params.playerName + ' WON! <br> Zdobyłeś: ' + params.playerScore + 'punkty/ów!';
     params.canPlay = false;
     disableButtons();
     displayGameResult(message, message2);
     displayFinalGameResult('', message2);
   } else if (params.computerScore == params.winsNumber) {
-    message2 = 'GAME OVER! Twój przeciwnik zwyciężył! Punkty które zdobył:<br> ' + params.computerScore;
+    message2 = 'GAME OVER! <br> Twój przeciwnik zwyciężył! <br> Punkty które zdobył: ' + params.computerScore;
     params.canPlay = false;
     disableButtons();
     displayGameResult(message, message2);
     displayFinalGameResult('', message2);
   } else {
-    message2 = '<strong>Round</strong> no ' + params.roundNumber + '<br>' + '<strong>Player1 result <br></strong>' + params.playerScore + '<br>' + '<strong>Player2 result</strong> <br>' + params.computerScore + '<br> Gramy do ' + params.winsNumber;
+    message2 = '<strong>Round no ' + params.roundNumber + '</strong> <br><br>' + '<strong>' + params.playerName + '<br></strong>' + params.playerScore + '<br>' + '<strong>Computer result</strong> <br>' + params.computerScore + '<br> Gramy do ' + params.winsNumber;
     displayGameResult(message, message2);
   }
 
